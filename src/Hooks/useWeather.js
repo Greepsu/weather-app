8b0799b19fa5a3d428eb7e7cbf60c6a3d5192deb
Greepsu <arnaud.mgn2@gmail.com>
2021-10-21 13:16:25 +0200

    Fix

--- src/Hooks/useWeather.js
+++ src/Hooks/useWeather.js
@@ -1,35 +1,37 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
 const useWeather = (query) => {
-    const [result, setResult] = useState([]);
-    const [loading, setLoading] = useState("false");
-    const API_KEY = 'bd4fde425735192346803102941804ec';
+  const [result, setResult] = useState([]);
+  const [loading, setLoading] = useState("false");
+  const API_KEY = process.env.REACT_APP_API_KEY;
 
-  
-    useEffect(() => {
-      async function fetchWeather() {
-        try {
-          setLoading("true");
-          const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}&units=metric`);
-          const json = await response.json();
-          console.log(json);
-          setResult(
-            json.items.map(item => {
-              console.log(item);
-              return item
-            })
-          );
-        } catch (error) {
-          setLoading("null");
-        }
+  useEffect(() => {
+    async function fetchWeather() {
+      try {
+        setLoading("true");
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}&units=metric`
+        );
+        const json = await response.json();
+        console.log(json);
+        setResult(
+          json.items.map((item) => {
+            console.log(item);
+            return item;
+          })
+        );
+      } catch (error) {
+        setLoading("null");
       }
-  
-      if (query !== "") {
-        fetchWeather();
-      }
-    }, [query]);
-  
-    return [result, loading];
-  }
+    }
 
-  export default useWeather
+    if (query !== "") {
+      fetchWeather();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query]);
+
+  return [result, loading];
+};
+
+export default useWeather;
